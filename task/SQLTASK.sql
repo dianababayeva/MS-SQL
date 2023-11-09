14ea116de695724bd257e2a7bd9fdad34c1a1ed6
dianababayeva <babayeva.diana2002@gmail.com>
2023-11-10 01:26:19 +0400

    task

---
+++ task/SQLTASK.sql
@@ -0,0 +1,29 @@
+
+--insert into Students (FullName , grade , birthdate , groupNo)
+--values 
+--('AGAZADE LEYLA' , 99 , 15.09 , 111)
+--drop table Students
+--create table Students (
+--FullName nvarchar,
+--grade int,
+--birthdate datetime,
+--groupNo int
+--)
+--DROP TABLE Students
+--CREATE TABLE STUDENTS(
+--FULLNAME NVARCHAR(50),
+--GRADE INT ,
+--BIRTHDATE DATETIME ,
+--GROUPNO INT 
+--)
+--insert into Students (FullName , grade , birthdate , groupNo)
+--values 
+--('AGAZADE LEYLA' , 99 , 15.09 , 111),
+--('BABAYEVA ELVIRA' , 57 , 14.11 , 111),
+--('QURBANOVA ZULEYXA',49 , 20.08 , 110),
+--('MAHMUDOV PARVIZ' , 75 , 23.12 , 111),
+--('CAVID BABAYEV' , 25 , 09.09 , 110)
+
+--select * from Students
+
+
